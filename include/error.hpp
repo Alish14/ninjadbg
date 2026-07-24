2d126e7aff65f8ca587fbcd1d1395cfcb2f04897
alish014 <alish14.mod@gmail.com>
2026-07-24 22:00:36 +0330

    first commit and setup CMake

--- include/error.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdexcept>
-#include <string>
-
-class DebuggerError : public std::runtime_error {
-public:
-    explicit DebuggerError(const std::string& msg)
-        : std::runtime_error(msg) {}
-};
